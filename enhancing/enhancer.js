0f2c792ada761a4e2e42845d3b7fb2576d74f55c
jalvaradoWD <jalvarado1337@lavabit.com>
2021-01-19 23:01:56 -0600

    Finished with the project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,57 @@
-module.exports = {
-  success,
-  fail,
-  repair,
-  get,
-};
+// A class to create a new Item object
+class Item {
+  constructor(props) {
+    const { name, durability, enhancement } = { ...props };
+    this.name = name;
+    this.durability = durability || Math.floor(Math.random() * 10 + 1);
+    this.enhancement = enhancement || Math.floor(Math.random() * 20 + 1);
+  }
+
+  repair() {
+    this.durability = 100;
+  }
+
+  success() {
+    if (this.enhancement !== 20) {
+      this.enhancement += 1;
+    }
+  }
+
+  fail() {
+    if (this.enhancement < 15) {
+      this.durability -= 5;
+    } else if (this.enhancement >= 15) {
+      this.durability -= 10;
+      if (this.enhancement > 16) {
+        this.enhancement -= 1;
+      }
+    }
+  }
+}
 
 function success(item) {
+  item.success();
   return { ...item };
 }
 
 function fail(item) {
+  item.fail();
   return { ...item };
 }
 
 function repair(item) {
+  item.repair();
   return { ...item };
 }
 
 function get(item) {
   return { ...item };
 }
+
+module.exports = {
+  success,
+  fail,
+  repair,
+  get,
+  Item,
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,127 @@
 const enhancer = require('./enhancer.js');
+// const { TestScheduler } = require('jest');
+
 // test away!
+
+test('Check if item is repaired to the value of 100', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 10,
+  });
+
+  const expectedValue = 100;
+
+  // execute the code on the test
+
+  enhancer.repair(testItem);
+  const actualValue = testItem.durability;
+
+  // assert results
+
+  expect(actualValue).toBe(expectedValue);
+});
+
+test('Check to see if enhancement succeeds with a enhancement value below 20.', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 15,
+  });
+
+  const expectedValue = 16;
+
+  // execute the code on the test
+
+  enhancer.success(testItem);
+  const actualValue = testItem.enhancement;
+
+  // assert results
+
+  expect(actualValue).toBe(expectedValue);
+});
+
+test('Check to see if enhancement succeeds with a enhancement value with 20. If so that value stays at 20.', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 20,
+  });
+
+  const expectedValue = 20;
+
+  // execute the code on the test
+
+  enhancer.success(testItem);
+  const actualValue = testItem.enhancement;
+
+  // assert results
+
+  expect(actualValue).toBe(expectedValue);
+});
+
+test('When enhancement fail with the value less that 15, durability decreases by 5.', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 10,
+  });
+
+  const expectedValue = 45;
+
+  // execute the code on the test
+
+  enhancer.fail(testItem);
+  const actualValue = testItem.durability;
+
+  // assert results
+
+  expect(actualValue).toBe(expectedValue);
+});
+
+test('When enhancement fail with the value is or greater than 15, durability decreases by 10.', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 15,
+  });
+
+  const expectedValue = 40;
+
+  // execute the code on the test
+
+  enhancer.fail(testItem);
+  const actualValue = testItem.durability;
+
+  // assert results
+
+  expect(actualValue).toBe(expectedValue);
+});
+
+test('When enhancement fail with the value greater than 16, enhancement decreases by 1.', () => {
+  // setup
+  const testItem = new enhancer.Item({
+    name: 'A new Item',
+    durability: 50,
+    enhancement: 17,
+  });
+
+  const expectedValueEnhancement = 16;
+  const expectedValueDurability = 40;
+
+  // execute the code on the test
+
+  enhancer.fail(testItem);
+  const actualValueEnhancement = testItem.enhancement;
+  const actualValueDurability = testItem.durability;
+
+  // assert results
+
+  expect(actualValueEnhancement).toBe(expectedValueEnhancement);
+  expect(actualValueDurability).toBe(expectedValueDurability);
+});
